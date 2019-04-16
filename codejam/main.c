18db8bcf32abffc25b629e269e8814f31d58fadb
Marian-Cristian Rotariu <marian.c.rotariu@gmail.com>
2019-04-17 00:27:33 +0200

    codejam: partial solve of the CJ2019 R1A #2
    Signed-off-by: Marian-Cristian Rotariu <marian.c.rotariu@gmail.com>

--- codejam/main.c
+++ codejam/main.c
@@ -11,6 +11,7 @@
 int T, N, M;
 int p[7] = {2, 3, 5, 7, 11, 13, 17};
 int R[MAXN][18];
+int Rt[MAXN];
 char l1[10000];
 char l2[10000];
 
@@ -28,21 +29,25 @@ int letters(int a)
 
 int main()
 {
-    FILE *f = fopen("debug.txt", "w");
+    //FILE *f = fopen("debug.txt", "w");
 
     int t;
     scanf("%d %d %d", &T, &N, &M); 
-    fprintf(f, "T=%d\n", T);
-    fprintf(f, "N=%d\n", N);
-    fprintf(f, "M=%d\n", M);
-    fflush(f);
+    //fprintf(f, "T=%d\n", T);
+    //fprintf(f, "N=%d\n", N);
+    //fprintf(f, "M=%d\n", M);
+    //fflush(f);
 
     int i, j;
     int res;
     int rindex;
+    int found;
 
     t = 0; 
     while (t < T) {
+	//fprintf(f, "Test case #%d\n", t);
+	//fflush(f);
+
 	for (i = 0; i < N; i++) {
 
 	    memset(l1, 0, sizeof(char) * 10000);
@@ -51,43 +56,67 @@ int main()
 	    for (j = 0; j < 18; j++) {
 		sprintf(l1, "%s %d", l1, p[i % 7]);
 	    }
-	    //sprintf(l1, "%s\n", l1);
 
-	    fprintf(f, "Q:%s\n", l1);
-	    fflush(f);
+	    //fprintf(f, "Q:%s\n", l1);
+	    //fflush(f);
 
 	    printf("%s\n", l1);
 	    fflush(stdout);
 
-	    //scanf("%s\n", l2);
-
-	    //fprintf(f, "R: %s\n", l2);
-	    //fflush(f);
-
 	    rindex = 0;
-	    fprintf(f, "R: ");
+	    //fprintf(f, "R: ");
+	    //fflush(f);
 	    for (j = 0; j < 18; j++) {
 		scanf("%d", &R[i][j]);
-		//sscanf(l2 + rindex, "%d", &R[i][j]);
-		//fprintf(f, "%s\n", l2 + rindex);
-		//rindex += letters(R[i][j]) + 1;
-		fprintf(f, "%d ", R[i][j]);
+		fflush(stdin);
+
+		//fprintf(f, "%d ", R[i][j]);
+		//fflush(f);
 	    }
 
-	    fprintf(f, "\n");
-	    fflush(f);
+	    //fprintf(f, "\n");
+	    //fflush(f);
 	}
 
-	printf("100\n");
-	fflush(stdout);
+	for (i = 0; i < 7; i++) {
+	    Rt[i] = 0;
+	    for (j = 0; j < 18; j++) {
+		Rt[i] += R[i][j];
+	    }
+	}
+
+	for (i = 1; i <= M; i++) {
+	    found = 1;
+	    for (j = 0; j < 7; j++) {
+		if ((i < Rt[j]) || (i - Rt[j]) % p[j]) {
+		    found = 0;
+		    break;
+		}
+	    }
+
+	    if (found) {
+		printf("%d\n", i);
+		fflush(stdout);
+
+		//fprintf(f, "cand: %d\n", i);
+		//fflush(f);
+
+		break;
+	    }
+	}
+
 	scanf("%d", &res);
 
-	fprintf(f, "%d\n", res);
+	//fprintf(f, "res: %d\n", res);
+	//fflush(f);
+
+	if (res == -1)
+	    return 0;
 
 	t++;
     }
 
-    fclose(f);
+    //fclose(f);
 
     return 0;
 }
